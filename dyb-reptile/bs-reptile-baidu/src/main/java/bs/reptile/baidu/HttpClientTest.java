7e455b0ce2ceda03bd2084ff905e4de5807cbde2
戴雁冰 <7457322@qq.com>
2024-01-25 11:54:44 +0800

    封装方法

--- dyb-reptile/bs-reptile-baidu/src/main/java/bs/reptile/baidu/HttpClientTest.java
+++ dyb-reptile/bs-reptile-baidu/src/main/java/bs/reptile/baidu/HttpClientTest.java
@@ -6,10 +6,14 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.io.SAXReader;
 
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class HttpClientTest {
@@ -37,19 +41,53 @@ public class HttpClientTest {
     }
 
     public void getInfo() {
+//        String html = getUrl("https://www.taobao.com/");
+//        taText.setText(html);
+        Document document = loadFile("E:\\projects\\Doc\\test.html");
+
+    }
+
+    public String getUrl(String url) {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://www.taobao.com/");
-        httpGet.setHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
-        CloseableHttpResponse response = null;
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+        String content = "";
         try {
-            response = httpClient.execute(httpGet);
+            CloseableHttpResponse response = httpClient.execute(httpGet);
             if (response.getStatusLine().getStatusCode() == 200) {
-                String content = EntityUtils.toString(response.getEntity(), "UTF-8");
-                System.out.println(content);
+                content = EntityUtils.toString(response.getEntity(), "UTF-8");
             }
+            response.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+        return content;
+    }
+
+    public Document loadFile(String file) {
+        SAXReader sr = new SAXReader();
+        Document doc = null;
+        try {
+            doc = sr.read(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return doc;
+    }
+
+    public Document loadHtml(String html) {
+        return loadHtml(html, "UTF-8");
+    }
+
+    public Document loadHtml(String html, String encode) {
+        SAXReader sr = new SAXReader();
+        Document doc = null;
+        try {
+            doc = sr.read(new ByteArrayInputStream(html.getBytes(encode)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return doc;
     }
 
     public void run() {
